2f02c7c2feba37992e427efb95c25a9763ebd0ff
Sthephanny Santos <83566632+sthecss@users.noreply.github.com>
2022-10-16 23:17:24 -0300

    02
    Save do trabalho.

--- LAB 02/53 exercicios.c
+++ LAB 02/53 exercicios.c
@@ -1,8 +1,42 @@
+/*  Lista com 53 Exercicios da Disciplina PP.
+    Sthephanny C.S Santos.
+    Uberlandia 2022.
+
+    Adendo- Deixei cada um dos codigos entre comentarios de paragrafo para que a reversao do algoritmo em codigos uteis seja mais tranquilo para a compilacao. Espero que facilite ao inves de atrapalhar :) Aceito feedback caso tenha ficado ruim.
+*/
+
+// ==================== Exercicio 01  ==================== 
+// Faça um programa que leia um número inteiro e o imprima.
+/*
+#include <stdio.h>
+#include <stdio.h>
+int main (){
+
+    int inteiro=225;
+    printf("O numero a seguir eh exemplo de um numero inteiro: %d \n", inteiro);
+
+    printf("Fim do programa!\n");
+    system ("pause");
+    return 0;
+}
+*/
+// ====================================================== 
+
+
+// ==================== Exercicio 02 ====================
+// Programa que leia um número real e o imprima.
+/*
+#include <stdio.h>
 #include <stdio.h>
 
-int main()
-{
-  printf("Hello World!\n");
+void main (){
 
-  return 0;
+    float real=141.5342;
+    printf("O numero a seguir eh exemplo de um numero real: %f\n", real);
+
+    printf("Fim do programa!\n");
+    system ("pause");
+    return 0;
 }
+*/
+// ======================================================
