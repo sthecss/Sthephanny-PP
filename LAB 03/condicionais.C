f6896e3fdac207c5fe34a6658c945c1fc1a26811
Sthephanny Santos <83566632+sthecss@users.noreply.github.com>
2022-10-21 10:36:14 -0300

    Update condicionais.C

--- LAB 03/condicionais.C
+++ LAB 03/condicionais.C
@@ -1,29 +1,434 @@
+/* 
+  ALuna       : Sthephanny Caroline da Silva Santos;
+  Matricula   : 12211BCC044;
+  lAB 03      : CONDICIONAIS;
+  Recebido    : 21/10
+  Entrega     : 
+  Uberlandia 2022.
+*/
+
+
+// ==================== Exercicio 01 =====================
+// Faça um programa que receba dois números e mostre qual deles é o maior.
+/*
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int fatorial (int n) {
-int i,f=1;
-for (i = 1; i <= n; i++)
-  f = f * i;
-  return f;
-  
-  }
-int main () {
-  int x,y;
-  printf("Digite um valor inteiro: ");
-  scanf("%d",&x);
-      if (x > 0){
-      printf("X eh positivo\n");
-      y = fatorial (x);
-      printf("Fatorial de X eh %d\n",y);
-    }else{
-      if (x < 0)
-        printf("X eh negativo\n");
-      else
-        printf("X eh Zero\n");
+int main (){
+
+    int x1, x2;
+    printf("Digite dois numeros reais, e sera mostrado o de maior valor: ");
+    scanf("%d""%d", &x1, &x2);
+
+    if (x1 > x2) {
+      printf("O maior eh: %d.\n", x1);
     }
 
-  printf ("Fim do programa!\n");
-  system ("pause");
-  return 0;
+    if (x2 > x1) {
+      printf("O maior eh: %d.\n", x2);
+    }
+
+    printf("\nFim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 02 =====================
+// Leia um número fornecido pelo usuário. Se esse número for positivo, calcule a raiz quadrada do número. Se o número for negativo, mostre uma mensagem dizendo que o número é inválido.
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+    float num, raiz_quad;
+    printf("\nDigite um valor: ");
+    scanf("%f", &num);
+
+    if (num > 0){
+      raiz_quad = sqrt(num);
+      printf("A raiz quadrada desse numeros eh de:%.4f\n", raiz_quad);
+    }
+
+    if (num < 0){
+      printf("ERRO. O numero que digitou eh negativo.\n");
+    }
+
+    printf("\nFim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 03 =====================
+// Leia um número real. Se o número for positivo imprima a raiz quadrada. Do contrário, imprima o número ao quadrado.
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+    float num, raiz_quad, expo;
+    printf("\nDigite um valor real: ");
+    scanf("%f", &num);
+
+    if (num > 0){
+      raiz_quad = sqrt(num);
+      printf("A raiz quadrada desse numeros eh de: %.4f\n", raiz_quad);
+    }
+
+    if (num < 0){
+      expo = pow(num, 2); 	
+      printf("Esse numero ao quadrdo eh de: %.f\n", expo);
   }
+
+    printf("\nFim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 04 =====================
+/* Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre:
+- O número digitado ao quadrado;
+- A raiz quadrada do número digitado.
+*/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+    float num, raiz_quad, expo;
+    printf("\nDigite um valor real: ");
+    scanf("%f", &num);
+
+    if (num > 0){
+      raiz_quad = sqrt(num);
+        printf("A raiz quadrada desse numeros eh de: %.4f\n", raiz_quad);
+      expo = pow(num, 2); 	
+        printf("Esse numero ao quadrdo eh de: %4.f\n", expo);
+    }
+
+    if (num < 0){
+      printf("ERRO. O numero que digitou eh negativo. Redigite.\n");
+    }
+
+    printf("\nFim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 05 =====================
+// Faça um programa que receba um número inteiro e verifique se este número é par ou ímpar.
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+    int num, zero;
+    zero= 0;
+
+    printf("\nDigite um numero inteiro: ");
+    scanf("%d", &num);
+
+    if (num%2 == 0){
+      printf("O numero digitado eh par.\n", num);
+    }
+
+    if ((num % 2) != zero){
+      printf("O numero eh impar.\n", num);
+    }
+
+    printf("\nFim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 06 =====================
+// 
+///*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+//*/
+// ======================================================
+
+
+// ==================== Exercicio 07 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 08 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 09 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 10 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 11 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 12 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 13 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 14 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 15 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 16 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+//*/
+// ======================================================
+
+
+// ==================== Exercicio 17 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 18 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 19 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
+
+
+// ==================== Exercicio 20 =====================
+// 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+
+    printf("Fim do programa!\n\n");
+    system("pause");
+    return 0;
+} 
+*/
+// ======================================================
